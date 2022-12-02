f2f95cb3b2a3ada2b1f5b5906b328eee8ffb8e8d
Gaurav Asode <gauravasode5@gmail.com>
2022-12-02 09:04:58 +0530

    check_fix

--- plaindrome.js
+++ plaindrome.js
@@ -2,10 +2,10 @@ let str = "namaman";
 let ans = false;
 let j = Math.floor(str.length/2); 
 for(let i=0; i<=j; i++){
-    if(str[i] != str[str.length-i-1]){
+    if(str[i] == str[str.length-i-1]){
         ans = true;
     }else{
-        break;
+      break;
     }
 }
 console.log(ans)
--- prime_number.js
+++ prime_number.js
@@ -1,12 +1,8 @@
 let num = 41;
 let count = 0;
 for(let i=1; i<=num; i++){
-    if(num&2 == 0){
+    if(num&i == 0){
         count++;
     }
 }
-if(count == 2){
-    console.log(false);
-}else{
-    console.log(false);
-}
+(count==2) ? console.log("true") : console.log("false")
